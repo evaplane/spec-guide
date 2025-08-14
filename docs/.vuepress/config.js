1361e8921a31c9ff446f7f98ba2f8b6b7afb7f71
liy54 <liy@54mingyuanyun.com>
2025-08-14 13:15:55 +0800

    init vuepress lerna

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { webpackBundler } from '@vuepress/bundler-webpack'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'VuePress',
+  description: 'My first VuePress Site',
+
+  theme: defaultTheme({
+    logo: '/img/img1.png',
+
+    navbar: ['/', '/get-started'],
+  }),
+
+  bundler: webpackBundler(),
+})
