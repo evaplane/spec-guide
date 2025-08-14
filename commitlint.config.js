c7463854b55bdcf8da2f0543f4757ef0a8ae74be
liy54 <liy@54mingyuanyun.com>
2025-08-14 17:05:06 +0800

    fix: 再试一次commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
-export default { extends: ['@commitlint/config-conventional'] };
+// export default { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['./packages/commitlint-config/index.js'],
+};
